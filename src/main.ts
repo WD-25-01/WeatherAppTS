19e1e227c8a9eb751924c13dcca2746f20b00ed4
Elisaatnam <elisa.stueker@googlemail.com>
2025-03-17 09:58:37 +0100

    env test

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,7 @@
 import { GeoData, WaetherData } from "./types/types";
 
-const apiKey = "bdcaff9cf7896aa03cd665ece7d83d76";
-
-// const apiKey = import.meta.env.VITE_OPENWEATHER_API_KEY;
-// console.log("API KEY", apiKey);
+const apiKey = import.meta.env.VITE_OPENWEATHER_API_KEY;
+console.log("API KEY", apiKey);
 
 const searchResultDiv = document.querySelector("#search-results");
 
